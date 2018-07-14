96d3a96a1f3942365afb12549bb2ac637cc2923d
Michael Orwin <mcokzoo@icloud.com>
2018-07-14 01:33:37 -0400

    shorten ride

--- chapter_1/section_2/ride/python2/ride.py
+++ chapter_1/section_2/ride/python2/ride.py
@@ -1,13 +1,17 @@
-"""
+'''
 ID: mcokzoo1
 LANG: PYTHON2
 TASK: ride
-"""
-with open('ride.in', 'r') as fin, open('ride.out', 'w') as fout:
-	group = 1
-	comet = 1
-	for c in fin.readline():
-		group *= ord(c)-64
-	for c in fin.readline():
-		comet *= ord(c)-64
-	fout.write('GO\n') if (group-comet) % 47 == 0 else fout.write('STAY\n')
+'''
+
+def eval_name(s):
+	p = 1
+	for c in s:
+		p *= ord(c)-64
+	return p
+
+if __name__ == '__main__':
+	with open('ride.in', 'r') as fin, open('ride.out', 'w') as fout:
+		group = fin.readline()
+		comet = fin.readline()
+		fout.write('GO\n') if (eval_name(group)-eval_name(comet)) % 47 == 0 else fout.write('STAY\n')
